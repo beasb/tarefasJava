f18a30b206a700ee9f4ebd676a2c1e2bc988ab7b
beasb <bia4sb4@gmail.com>
2024-12-03 08:32:21 -0300

    tarefas

---
+++ src/tarefaJava/OperadoresLogicos.java
@@ -0,0 +1,14 @@
+package tarefaJava;
+
+public class OperadoresLogicos {
+
+	public static void main(String[] args) {
+		
+		int a = 3;
+		int b = 6;
+		
+		System.out.println(!(a == 2 || b == 7));
+
+	}
+
+}
